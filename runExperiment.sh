046d09e1149c201fc828b91923db7c4e5f2eff11
Lasse Wolter <wolter.lasse@gmail.com>
2020-02-15 13:26:12 +0100

    Added the possibility of using a costum docker image
    + allows to use a local docker image for the runExperiment script

--- runExperiment.sh
+++ runExperiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ];
 then
-    echo "Usage $0 <expConfig> <queueLogDir> <expName> <expId>"
+    echo "Usage $0 <expConfig> <queueLogDir> <expName> <expId> <dockerImage>"
     exit 
 fi
 
@@ -10,6 +10,9 @@ queueLogDir=$2
 expName=$3
 expId=$4
 curExpDir="./curExps/"
+
+# Possibility of using a costum docker image
+dockerImage=${5:-"crosscoder/mix_net:v2"}
 
 echo "---------------------------------------"
 printf "%s\n" "----------Experiment: $expName"
@@ -36,12 +39,11 @@ printf "Starting docker container...\n"
 # The --rm flag causes the docker container to be removed after it's finished running and thus, not waste space on the local machine
 docker run --rm -v $PWD/$queueLogDir/$expName:/mix_net/queue_outputs/$expName \
            -v $PWD/$curExpDir/exp${expId}:/cliConf \
-           crosscoder/mix_net:v2 
-
+           $dockerImage
 # If no log directory was created this means that this iteration exited early - thus, rerun this iteration
 if [ ! -d "$queueLogDir/$expName/exp$expId/log" ]; then
     echo "Something went wrong, rerunning this experiment iteration..."
-    runExperiment.sh $1 $2 $3 $4
+    ./runExperiment.sh $1 $2 $3 $4 $5
 fi
 #printf "Experiment done, creating plots..."
 #./queue_outputs/makePlot.sh ./queue_outputs/$expName 
